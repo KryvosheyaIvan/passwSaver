59143c638c11568136c0f686eb970a37c9165f76
KryvosheyaIvan <kryvosheia.ivan@gmail.com>
2019-03-11 23:18:38 +0200

    starting main window with AppIcon, username , password text fields and buttons added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QPixmap>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    QPixmap pix("D:/QTprojects/pswdSaver/pswdSaver/keysApp.png");
+    ui->pictLabel->setPixmap(pix.scaled(150,150,Qt::KeepAspectRatio));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_loginButton_clicked()
+{
+    /* Getting password and username when #login button is clicked */
+    QString appUsername = ui->userLineEdit->text();
+    QString appPassword = ui->passwLineEdit->text();
+
+    if(appUsername == "test" && appPassword == "test") {
+        QMessageBox::information(this, "Login", "Username and password is correct");
+    }
+    else {
+        QMessageBox::warning(this, "Login", "Username and password is wrong!");
+    }
 }
