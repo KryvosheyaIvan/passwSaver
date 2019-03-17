af18d6febd919159db9767954e2d7cd7d9b59094
KryvosheyaIvan <kryvosheia.ivan@gmail.com>
2019-03-17 22:49:16 +0200

    some succes in json parsing in qt

--- modules/userProfiles/userprofiles.cpp
+++ modules/userProfiles/userprofiles.cpp
@@ -3,6 +3,7 @@
 #include <QDebug>
 #include <QJsonObject>
 #include <QJsonDocument>
+#include <QJsonArray>
 #include <QFileDialog>
 #include <QFile>
 #include <QCoreApplication>
@@ -32,21 +33,58 @@ bool userProfiles::addUserProfile(QString username, QString pswd, QWidget *paren
     QString appDir = QCoreApplication::applicationDirPath();
     QFile file(appDir + "/users.json");
 
-    if ( !file.open(QIODevice::WriteOnly/* | QIODevice::Text*/)) {
+    if ( !file.open(QIODevice::ReadOnly /*WriteOnly*/ | QIODevice::Text)) {
         qDebug() << "addUserProfile: failed to open file" << endl;
         return false;
     }
+    QString strFile = file.readAll();
+
+    //first need to read from file and then append
+    QJsonDocument jsReadDoc;
+    //jsReadDoc = QJsonDocument::fromJson(file.readAll());
+    jsReadDoc = QJsonDocument::fromJson(strFile.toUtf8());
+
+    if (jsReadDoc.isArray()) {
+        qDebug() << "doc is array" << endl;
+    }
+    else {
+        qDebug() << "doc is not an array" << endl;
+    }
+
+    if (jsReadDoc.isEmpty()) {
+        qDebug() << "doc is empty" << endl;
+    }
+    else {
+        qDebug() << "doc is not an empty" << endl;
+    }
+
+    QByteArray byteArray = jsReadDoc.toJson();
+    qDebug() << byteArray << endl;
+
+
 
     QJsonObject jsObject;
     jsObject["username"] = username;
     jsObject["password"] = pswd;
 
+    QJsonValue jsValue(jsObject);
+
+    QJsonArray jsArray;
+    jsArray.append(jsValue);
 
 
-    QJsonDocument jsDoc(jsObject);
+
+
+    file.close();
+    if ( !file.open(QIODevice::WriteOnly/* | QIODevice::Text*/)) {
+        qDebug() << "addUserProfile: failed to open file 2" << endl;
+        return false;
+    }
+
+    QJsonDocument jsDoc(jsArray);
     file.write(jsDoc.toJson());
-    //jsDoc.toJson();
-    //jsDoc
+    file.close();
+
 
     //file.write(jsDoc.toJson());
 
